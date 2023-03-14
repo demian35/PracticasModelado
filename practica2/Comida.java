afa93fa33b910f8f1c4ceb8c7a6e7fe7028d2ea3
Demian <53890358+demian35@users.noreply.github.com>
2023-03-14 02:42:20 -0600

    clase comida

---
+++ practica2/Comida.java
@@ -0,0 +1,17 @@
+package practica2;
+
+public class Comida implements Preparable {
+
+    private float temp;
+
+    public Comida(float temperaturaInicial){
+        temp=temperaturaInicial;
+    }
+
+    @Override
+    public void preparaAlimento(float temp) {
+        this.temp += temp;
+    }
+
+
+}
