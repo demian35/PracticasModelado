7d0b790be2fe54e701784f3fbea183325f35baa1
Demian <53890358+demian35@users.noreply.github.com>
2023-03-14 03:15:42 -0600

    implementacion de la interfaz que controlara la estufa

--- practica2/InterfazEstufa.java
+++ practica2/InterfazEstufa.java
@@ -1,5 +1,27 @@
 package practica2;
 
 public class InterfazEstufa {
-    
+    private Estufa estufa;
+
+    public InterfazEstufa(Estufa estufa){
+        this.estufa=estufa;
+    }
+
+    public boolean calientaAlimento(Preparable comida){
+        return estufa.actualizaParrillas(comida, "Prepara");
+        
+    }
+
+    public boolean quitaAlimento(Preparable comida){
+        return estufa.actualizaParrillas(comida,"Comida Lista" );
+    }
+
+    public boolean prendeBrasa(){
+        return estufa.estadoEstufa("Braza encendida");
+    }
+
+    public boolean apagaBraza(){
+        return estufa.estadoEstufa("Braza apagada");
+    }
+
 }
